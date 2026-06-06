416ac5cdd105e8544e2f88e326b9f07d3c260265
abdo-essam <abdo.e.mosa@gmail.com>
2026-06-06 03:15:34 +0300

    fix(core): add Dispatchers import to PluginManager
    Adds the `kotlinx.coroutines.Dispatchers` import to `PluginManager.kt`.

--- core/src/commonMain/kotlin/com/ae/log/plugin/PluginManager.kt
+++ core/src/commonMain/kotlin/com/ae/log/plugin/PluginManager.kt
@@ -4,6 +4,7 @@ import com.ae.log.config.LogConfig
 import kotlinx.atomicfu.locks.SynchronizedObject
 import kotlinx.atomicfu.locks.synchronized
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
